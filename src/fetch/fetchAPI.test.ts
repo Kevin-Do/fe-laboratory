864e8452b1a97711499f5189fea2ced0604619bf
Kevin Do <kevindokhoale@gmail.com>
2023-04-11 21:35:11 -0700

    Comment out fetch examples

--- src/fetch/fetchAPI.test.ts
+++ src/fetch/fetchAPI.test.ts
@@ -1,53 +1,54 @@
-// api.test.ts
-import FetchAPI from './fetchAPI';
+// /* eslint-disable @typescript-eslint/no-unsafe-call */
+// // api.test.ts
+// import FetchAPI from './fetchAPI';
 
-// Mock the global fetch function
-global.fetch = jest.fn();
+// // Mock the global fetch function
+// global.fetch = jest.fn();
 
-describe('FetchAPI', () => {
-  const baseUrl = 'https://jsonplaceholder.typicode.com';
-  const api = new FetchAPI(baseUrl);
+// describe('FetchAPI', () => {
+//   const baseUrl = 'https://jsonplaceholder.typicode.com';
+//   const api = new FetchAPI(baseUrl);
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+//   afterEach(() => {
+//     jest.clearAllMocks();
+//   });
 
-  test('GET request', async () => {
-    // Mock fetch response
-    (fetch as jest.Mock).mockResolvedValueOnce({
-      ok: true,
-      json: async () => [{ id: 1, title: 'Test Post' }],
-    });
+//   test('GET request', async () => {
+//     // Mock fetch response
+//     (fetch as jest.Mock).mockResolvedValueOnce({
+//       ok: true,
+//       json: async () => [{ id: 1, title: 'Test Post' }],
+//     });
 
-    const response = await api.get('/posts');
+//     const response = await api.get('/posts');
 
-    expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith(`${baseUrl}/posts`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    });
-    expect(response).toEqual([{ id: 1, title: 'Test Post' }]);
-  });
+//     expect(fetch).toHaveBeenCalledTimes(1);
+//     expect(fetch).toHaveBeenCalledWith(`${baseUrl}/posts`, {
+//       method: 'GET',
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//     expect(response).toEqual([{ id: 1, title: 'Test Post' }]);
+//   });
 
-  test('POST request', async () => {
-    const postData = { title: 'New Post', body: 'This is a new post.' };
+//   test('POST request', async () => {
+//     const postData = { title: 'New Post', body: 'This is a new post.' };
 
-    // Mock fetch response
-    (fetch as jest.Mock).mockResolvedValueOnce({
-      ok: true,
-      json: async () => ({ id: 2, ...postData }),
-    });
+//     // Mock fetch response
+//     (fetch as jest.Mock).mockResolvedValueOnce({
+//       ok: true,
+//       json: async () => ({ id: 2, ...postData }),
+//     });
 
-    const response = await api.post('/posts', postData);
+//     const response = await api.post('/posts', postData);
 
-    expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith(`${baseUrl}/posts`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(postData),
-    });
-    expect(response).toEqual({ id: 2, ...postData });
-  });
+//     expect(fetch).toHaveBeenCalledTimes(1);
+//     expect(fetch).toHaveBeenCalledWith(`${baseUrl}/posts`, {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(postData),
+//     });
+//     expect(response).toEqual({ id: 2, ...postData });
+//   });
 
-  // You can add similar test cases for PUT and DELETE requests
-});
+//   // You can add similar test cases for PUT and DELETE requests
+// });
--- src/fetch/fetch_example.ts
+++ src/fetch/fetch_example.ts
@@ -1,27 +1,29 @@
-// main.ts
-import FetchAPI from './fetchAPI';
+// /* eslint-disable @typescript-eslint/restrict-template-expressions */
+// /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// // main.ts
+// import FetchAPI from './fetchAPI';
 
-const api = new FetchAPI('https://jsonplaceholder.typicode.com');
+// const api = new FetchAPI('https://jsonplaceholder.typicode.com');
 
-await (async () => {
-  try {
-    const posts = await api.get('/posts');
-    console.log(posts);
+// await (async () => {
+//   try {
+//     const posts = await api.get('/posts');
+//     console.log(posts);
 
-    const newPost = {
-      title: 'New Post',
-      body: 'This is a new post.',
-      userId: 1,
-    };
+//     const newPost = {
+//       title: 'New Post',
+//       body: 'This is a new post.',
+//       userId: 1,
+//     };
 
-    const createdPost = await api.post('/posts', newPost);
-    console.log(createdPost);
+//     const createdPost = await api.post('/posts', newPost);
+//     console.log(createdPost);
 
-    const updatedPost = { ...createdPost, title: 'Updated Post' };
-    await api.put(`/posts/${createdPost.id}`, updatedPost);
+//     const updatedPost = { ...createdPost, title: 'Updated Post' };
+//     await api.put(`/posts/${createdPost.id}`, updatedPost);
 
-    await api.delete(`/posts/${createdPost.id}`);
-  } catch (error) {
-    console.error(error);
-  }
-})();
+//     await api.delete(`/posts/${createdPost.id}`);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
